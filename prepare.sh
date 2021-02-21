bed12c3e7b14a70b74e78e1cb7f3c4a3280b8db2
wangxy918 <78683769+wangxy918@users.noreply.github.com>
2021-02-21 10:18:48 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -15,15 +15,17 @@ echo "Perpare Script [Start]"
 # Custom feed
 ##################################
 echo ""
-echo "Adding lienol xxx feeds"
-"src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
-"src-git OpenClash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
-echo "cat feeds.conf.default"
-cat feeds.conf.default
+#echo "Adding lienol xxx feeds"
+echo "Adding passwall,OpenClash feeds"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
+echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
 
 echo ""
 echo "add lean xxx feeds"
-echo sed -i "s/^#\(src-git helloworld .*\)$/\1/" feeds.conf.default
+sed -i "s/^#\(src-git helloworld .*\)$/\1/" feeds.conf.default
+
+echo "review feeds.conf.default file"
+cat feeds.conf.default
 
 echo ""
 echo "Updating feeds"
@@ -37,35 +39,35 @@ echo "Installing feeds"
 # Custom package
 ##################################
 
-echo ""
-echo "Using luci-theme-argon offical source code"
-echo rm -rf package/lean/luci-theme-argon
-echo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#echo ""
+#echo "Using luci-theme-argon offical source code"
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-echo ""
-echo "Downloading Custom packages"
-echo git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
-echo git clone --depth=1 https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-echo git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-echo mv package/OpenClash/luci-app-openclash package/luci-app-openclash
-echo rm -rf package/OpenClash
+#echo ""
+#echo "Downloading Custom packages"
+#git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone --depth=1 https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+#git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+#mv package/OpenClash/luci-app-openclash package/luci-app-openclash
+#rm -rf package/OpenClash
 
 cd $OPENWRT_PATH
 
 ##################################
 # Settings
 ##################################
-echo ""
-echo "Configuring ..."
+#echo ""
+#echo "Configuring ..."
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
-if [ ! -z "$OPENWRT_ROOT_PASSWORD" ]; then
-  echo "WARN: root password is changed from your secret, make sure you add 'OPENWRT_ROOT_PASSWORD' secret"
+#if [ ! -z "$OPENWRT_ROOT_PASSWORD" ]; then
+#  echo "WARN: root password is changed from your secret, make sure you add 'OPENWRT_ROOT_PASSWORD' secret"
   # Modify password of root if present (encoded password)
   # For example: passwot = $1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.
-  sed -i "s|root::0:0:99999:7:::|root:$OPENWRT_ROOT_PASSWORD:0:0:99999:7:::|g" package/base-files/files/etc/shadow
-fi
+#  sed -i "s|root::0:0:99999:7:::|root:$OPENWRT_ROOT_PASSWORD:0:0:99999:7:::|g" package/base-files/files/etc/shadow
+#fi
 
 # Modify default theme
 # sed -i 's/bootstrap/argon/g' package/feeds/luci/luci-base/root/etc/config/luci
